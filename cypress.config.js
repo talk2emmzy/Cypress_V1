1beda3d03205b060dc32e3dd2b6ec855d15e02d4
Mirah <busarimariam.bm@gmail.com>
2023-10-02 15:31:29 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "nnrrzu",
+  reporter: 'cypress-mochawesome-reporter',
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+    },
+  },
+});
